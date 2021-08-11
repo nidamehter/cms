30ca15effd918cb785a653377427add99e1d06ec
Burakhan Şamlı <burakhansamli0.0.0.0@gmail.com>
2021-08-11 12:35:53 +0300

    composer ile gerekli kütüphaneler import edildi ve autoload indexe eklendi

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/controller.php';
 require __DIR__ . '/route.php';
 require __DIR__ . '/database.php';
 require __DIR__ . '/model.php';
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/helper.php';
 
 
