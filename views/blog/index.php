9daccc1db7af9e0ee8510fd9e83c829763a70ccb
nidamehter <69451860+nidamehter@users.noreply.github.com>
2021-08-16 16:52:15 +0300

    dropdown

--- views/blog/index.php
+++ views/blog/index.php
@@ -15,7 +15,16 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link  px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
                         <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
                     </ul>
@@ -41,73 +50,47 @@
                         </div>
                     </div>
                 </div>
-
             </header>
-            <nav class="floating-menu ">
-                <ul class="main-menu">
-                    <li>
-                    <a href="/cms/anasayfa/uzay" class="ripple">
-                            <i class="fas fa-home fa-lg"></i>
-                        </a>
-                    </li>
-                    
-                    <li>
-                        <a href="#" class="ripple">
-                            <i class="far fa-address-card fa-lg"></i>
-                        </a>
-                    </li>
-               
-                    <li>
-                    <a class="ripple" data-bs-toggle="offcanvas" href="#offcanvasExample"  aria-controls="offcanvasExample">
-                        <i class="fab fa-blogger-b fa-lg"></i>
-                        </a>
-                       
-                    </li>
-                </ul>
-                <div class="menu-bg"></div>
-            </nav>
 
             <!-- Main Content-->
-            <div class="container px-4 px-lg-5 top">
-
+            <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5 justify-content-center">
                     <div class="col-md-10 col-lg-8 col-xl-7">
 
+
                         <div id="sidebar-wrapper">
-                            <!--
-                            <div class="category-sidebar" >
-                            <button class="btn btn-primary"  type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                           << Kategoriler </button>
-                                </div> -->
-                            <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
-                                <div class="offcanvas-header">
-                                    <h5 class="offcanvas-title" id="offcanvasExampleLabel">Konu Başlıkları</h5>
-                                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                </div>
+
+                            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                                << Kategoriler </button>
+
+                                    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                                        <div class="offcanvas-header">
+                                            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Konu Başlıkları</h5>
+                                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                        </div>
 
 
-                                <div class="offcanvas-body offcanvasRenk">
-                                    <div class="overflow-auto">
-                                        <ul>
+                                        <div class="offcanvas-body offcanvasRenk">
+                                            <div class="overflow-auto">
+                                                <ul>
 
-                                            <?php foreach ($blogCategoryData as $key => $value) : ?>
+                                                    <?php foreach ($blogCategoryData as $key => $value) : ?>
 
-                                                <div class="list-group <?= $value['caturl'] == getLastPath(1) ? 'active' : null ?>">
-                                                    <a class="list-group-item list-group-item-action" href="#list-i
-                                                            em-1" v-on:click.prevent="getPosts" data-id="<?= $value['caturl'] ?>">
-                                                        <span class="far fa-bookmark"></span> <?= $value['name']; ?>
-                                                    </a>
-                                                </div>
+                                                        <div class="list-group <?= $value['caturl'] == getLastPath(1) ? 'active' : null ?>">
+                                                            <a class="list-group-item list-group-item-action" href="#list-item-1" v-on:click.prevent="getPosts" data-id="<?= $value['caturl'] ?>">
+                                                                <span class="far fa-bookmark"></span> <?= $value['name']; ?>
+                                                            </a>
+                                                        </div>
 
-                                            <?php endforeach; ?>
+                                                    <?php endforeach; ?>
 
-                                            <canvas class="space"></canvas>
-                                        </ul>
+                                                    <canvas class="space"></canvas>
+                                                </ul>
+                                            </div>
+
+
+                                        </div>
                                     </div>
-
-
-                                </div>
-                            </div>
                         </div>
 
                         <!-- Post preview-->
@@ -148,9 +131,8 @@
     </div>
     <!-- PageContent -->
 
-
     <!-- Bootstrap core JS-->
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Core theme JS-->
     <script src="js/scripts.js"></script>
@@ -158,15 +140,12 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
     <script src="blogCategories.js"></script>
+
     <!-- scripts -->
     <script src="js/particles/particles.js"></script>
     <script src="js/particles/app.js"></script>
     <script src="js/particles/lib/stats.js"></script>
     <script src="js/space/index.js"></script>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
 
 
 </body>
