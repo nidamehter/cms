2e1a3b78841d35d27d573f6885091026ed7f55c5
Burakhan Şamlı <burakhansamli0.0.0.0@gmail.com>
2021-08-17 22:28:10 +0300

    Update header.php

--- views/blog/header.php
+++ views/blog/header.php
@@ -5,6 +5,7 @@
     <base href="/cms/views/blog/" />
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1,  maximum-scale=1.0, user-scalable=no" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title><?= settings("blogTitle") ?></title>
