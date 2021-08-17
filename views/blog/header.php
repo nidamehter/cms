e069861210cd564bd2572f1318492b9d1556ed50
Burakhan Şamlı <burakhansamli0.0.0.0@gmail.com>
2021-08-17 10:45:34 +0300

    Revert "."
    This reverts commit 94fc7f4fa9ce31595f233649de49e14fd93d1dd6.

--- views/blog/header.php
+++ views/blog/header.php
@@ -15,6 +15,7 @@
     <!-- Google fonts-->
     <link href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css" />
+
     <!-- Core theme CSS (includes Bootstrap)-->
     <link href="css/styles.css" rel="stylesheet" />
     <link href="styles.css" rel="stylesheet" media="screen" />
@@ -22,17 +23,17 @@
     <!-- Extensions -->
     <link rel="stylesheet" media="screen" href="js/particles/style.css" />
     <link rel="stylesheet" href="js/space/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         
         body {
-            position: relative;z
+            position: relative;
         }
 
         .offcanvasRenk {
             background-image: url("assets/img/home-bg.jpg");
         }
 
-<<<<<<< Updated upstream
         .floating-menu {
             border-radius: 100px;
             z-index: 999;
@@ -45,18 +46,79 @@
             -ms-transform: translateY(-50%);
             transform: translateY(-50%);
             margin-top: 136px;
-=======
-        .make-me-sticky {
-            position: -webkit-sticky;
->>>>>>> Stashed changes
             position: sticky;
-            padding: 0 15px;
         }
 
-        .sidebar-item {
+        .top{
+        margin-top: -380px;
+
+        }
+        .main-menu {
+            margin: 0;
+            padding-left: 0;
+            list-style: none
+        }
+
+        .main-menu li a {
+            display: block;
+            padding: 20px;
+            color: #fff;
+            border-radius: 50px;
+           
+            -webkit-transition: none;
+            -o-transition: none;
+            transition: none
+        }
+
+        .main-menu li a:hover {
+            background: rgba(244, 244, 244, .3)
+        }
+
+        .menu-bg {
+            background-image: -webkit-linear-gradient(top, #1C5E91 0, #167699 100%);
+            background-image: -o-linear-gradient(top, #1C5E91 0, #167699 100%);
+            background-image: -webkit-gradient(linear, left top, left bottom, from(#1C5E91), to(#167699));
+            background-image: linear-gradient(to bottom, #1C5E91 0, #167699 100%);
+            background-repeat: repeat-x;
             position: absolute;
+            width: 100%;
+            height: 100%;
+            border-radius: 50px;
+            z-index: -1;
+            top: 0;
             left: 0;
-            text-align: center;
+            -webkit-transition: .1s;
+            -o-transition: .1s;
+            transition: .1s
+        }
+
+        .ripple {
+            position: relative;
+            overflow: hidden;
+            transform: translate3d(0, 0, 0)
+        }
+
+        .ripple:after {
+            content: "";
+            display: block;
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            pointer-events: none;
+            background-image: radial-gradient(circle, #000 10%, transparent 10.01%);
+            background-repeat: no-repeat;
+            background-position: 50%;
+            transform: scale(10, 10);
+            opacity: 0;
+            transition: transform .5s, opacity 1s
+        }
+
+        .ripple:active:after {
+            transform: scale(0, 0);
+            opacity: .2;
+            transition: 0s
         }
     </style>
 
